57f629cb4e70392d39c57acecf7ede641aac9a02
Telmo Fernandes <telmorf@live.com>
2023-10-19 11:51:42 +0100

    Add project files.

---
+++ 001-Xadrez-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_Xadrez_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
